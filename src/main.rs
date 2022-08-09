5d1208151d1980d35fd77de9e97230d9e4df1346
JohnLucid1 <SobolII21@st.ithub.ru>
2022-08-10 01:25:52 +0300

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-fn main() {
-    let img = image::open("src/wallpaper.png").unwrap();
+use qrcode::QrCode;
+use std::env;
 
-    println!("{}", img.to_rgb8());
+
+fn main() {
+    let args: Vec<String> =  env::args().collect();
+    if args.len() < 2 {
+        panic!("Not enough arguments");
+    }
+
+    let result = create_qr(&args[1]).unwrap();
+    println!("{}", &result);
+
+}
+
+fn create_qr(url: &String) -> Result<String, &'static str>{
+
+    if url.len() < 1{
+        return Err("Not Enough Arguments");
+    }
+
+    let code = QrCode::new(url.as_bytes()).unwrap();
+    let qrcode = code.render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+
+    Ok(qrcode)
+    
 }
